d24ca0548cc9362b69ea6b274fa6dba529f3aa97
Unknown15082 <trangiahuy15082006@gmail.com>
2025-05-29 21:29:13 +0700

    Restructure to initialize db in main file

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from profile import profile_page
-from db import db
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
 
 app.register_blueprint(profile_page)
-db.init_app(app)
+db = SQLAlchemy(app)
 
 with app.app_context():
     db.drop_all()
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,12 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(model_class=Base)
+from app import db
 
 
 class User(db.Model):

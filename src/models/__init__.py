442b108eb51d1fc29eb4c39acc1d49d40ac9548c
Unknown <39689724+Unknown15082@users.noreply.github.com>
2025-05-31 20:18:37 +0700

    Merge pull request #10 from ModulusAI-Orbital25/login
    Implemented #7, and with that, #2 as well.
    
        Added a password field to login
        Requiring a password field in the json data
        Changed the json key name for login: name -> username
        Updated database schema, added a password field, and added more constraints to other fields.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,13 +1,3 @@
-from app import db
-from flask_login import UserMixin
+from models.user import User
 
-
-class User(UserMixin, db.Model):
-    __tablename__ = "users"
-
-    id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column("username", db.String(50))
-    display_name = db.Column("display_name", db.String(100))
-
-    def serialize(self):
-        return {"id": self.id, "name": self.name, "display": self.display_name}
+__all__ = ["User"]

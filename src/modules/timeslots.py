f6223668130fc8283d911238b209f8fccb9625c0
Unknown <39689724+Unknown15082@users.noreply.github.com>
2025-06-28 14:42:56 +0700

    Use Google OR-Tools for constraint programming (#25)
    * Parse lessons data into timeslots
    
    * Converted list of modules into list of timeslots
    
    * Fix JSON format for timeslots data
    
    * Working optimizer

---
+++ src/modules/timeslots.py
@@ -0,0 +1,33 @@
+Lesson = dict[str, str | int | list[int]]
+Timeslot = tuple[int, int, int]
+
+
+def convert_day(dayName: str) -> int:
+    DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    return DAYS.index(dayName)
+
+
+def convert_time(timeValue: str) -> int:
+    hour = timeValue[:2]
+    return int(hour) - 8
+
+
+def lesson_to_timeslots(lesson: Lesson):
+    dateStr = lesson["day"]
+    startStr = lesson["startTime"]
+    endStr = lesson["endTime"]
+    weeks = lesson["weeks"]
+
+    assert isinstance(dateStr, str)
+    assert isinstance(startStr, str)
+    assert isinstance(endStr, str)
+    assert isinstance(weeks, list)
+
+    dateInt = convert_day(dateStr)
+    startInt = convert_time(startStr)
+    endInt = convert_time(endStr)
+
+    timeslots: list[Timeslot] = [
+        (w, dateInt, h) for w in weeks for h in range(startInt, endInt)
+    ]
+    return timeslots
